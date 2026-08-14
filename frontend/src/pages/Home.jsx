67ac78224ff654056730580b0d2fd6c7f79d3d7d
Full-C0mbo <camnic4908@gmail.com>
2026-08-14 05:56:32 -0400

    Frontend Skeleton
    Created skeleton text boxes for input and responses from backend, along with buttons for translation and copying.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import '../stylesheets/Home.css'
 
 function Home() {
+
+    // Define useStates for the textAreas.
+    const [userInput, setUserInput] = useState("");
+    const [response, setResponse] = useState("");
+
     return(
         <>
             <h1>Test</h1>
+
+            <div className='wrapper'>
+                <div className='input-area'>
+                    <textarea placeholder='Enter Text Here.' value={userInput} onChange={e => setUserInput(e.target.value)}/>
+                    <button>Translate</button>
+                </div>
+                
+                <div className='response-area'>
+                    <textarea placeholder='Response comes out here.' readOnly value={response} onChange={e => setResponse(e.target.value)}/>
+                    <button>Copy</button>
+                </div>
+            </div>
         </>
     );
 }
